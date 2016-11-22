831f439144b789511009b89ca6afa8971312e15a
Mario Bielert <mario.bielert@tu-dresden.de>
2016-11-22 10:18:13 +0100

    I don't know what the hell I was thinking.

--- cmake/Roco2Configuration.cmake
+++ cmake/Roco2Configuration.cmake
@@ -1,14 +1,18 @@
 
-macro(Roco2Configuration CONF)
-    add_executable(${CONF}/roco2 ${CONF}/experiment.cpp)
+macro(Roco2Configuration CONF )
+    add_executable(roco2_${CONF} ${CONF}/main.cpp)
 
-    target_link_libraries(${CONF}/roco2 ${LAPACK_LIBRARIES} ${LIBNUMA_LIBRARIES} ${MKL_LIBRARIES} firestarter fastcpufreq ${CPUFREQ_LIBRARIES} roco2_core)
+    target_link_libraries(roco2_${CONF} ${LAPACK_LIBRARIES} ${LIBNUMA_LIBRARIES} ${MKL_LIBRARIES} firestarter fastcpufreq ${CPUFREQ_LIBRARIES} roco2_core)
+
+    set_target_properties(roco2_${CONF} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/${CONF}")
 
     if(X86Adapt_FOUND)
-        target_link_libraries(${CONF}/roco2 ${X86ADAPT_LIBRARIES})
+        target_link_libraries(roco2_${CONF} ${X86ADAPT_LIBRARIES})
     endif()
 
-    add_scorep_instrumentation(${CONF}/roco2)
+    add_scorep_instrumentation(roco2_${CONF})
+    set_target_properties(roco2_${CONF}_scorep PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/${CONF}")
 
-    install(TARGETS ${CONF}/roco2 RUNTIME DESTINATION bin)
+
+    install(TARGETS roco2_${CONF} RUNTIME DESTINATION bin)
 endmacro()
