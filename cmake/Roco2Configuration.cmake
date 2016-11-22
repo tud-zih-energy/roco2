cdb7c1936da72bfff85413bb0b435cdda14e640f
Mario Bielert <mario.bielert@tu-dresden.de>
2016-11-22 10:11:29 +0100

    CMake magic

--- cmake/Roco2Configuration.cmake
+++ cmake/Roco2Configuration.cmake
@@ -1,14 +1,14 @@
 
-macro(Roco2Configuration CONF )
-    add_executable(roco2_${CONF} ${CONF}/main.cpp)
+macro(Roco2Configuration CONF)
+    add_executable(${CONF}/roco2 ${CONF}/experiment.cpp)
 
-    target_link_libraries(roco2_${CONF} ${LAPACK_LIBRARIES} ${LIBNUMA_LIBRARIES} ${MKL_LIBRARIES} firestarter fastcpufreq ${CPUFREQ_LIBRARIES})
+    target_link_libraries(${CONF}/roco2 ${LAPACK_LIBRARIES} ${LIBNUMA_LIBRARIES} ${MKL_LIBRARIES} firestarter fastcpufreq ${CPUFREQ_LIBRARIES} roco2_core)
 
     if(X86Adapt_FOUND)
-        target_link_libraries(roco2_${CONF} ${X86ADAPT_LIBRARIES} roco2_core)
+        target_link_libraries(${CONF}/roco2 ${X86ADAPT_LIBRARIES})
     endif()
 
-    add_scorep_instrumentation(roco2_${CONF})
+    add_scorep_instrumentation(${CONF}/roco2)
 
-    install(TARGETS roco2_${CONF} RUNTIME DESTINATION bin)
+    install(TARGETS ${CONF}/roco2 RUNTIME DESTINATION bin)
 endmacro()
