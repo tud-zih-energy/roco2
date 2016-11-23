55d0948a265776b3b8bce6511ba93acda87dc825
Mario Bielert <mario.bielert@tu-dresden.de>
2016-11-23 16:34:42 +0100

    Is it a coincidence, that bullshit and build system share an abbreviation? I think so.

--- cmake/Roco2Configuration.cmake
+++ cmake/Roco2Configuration.cmake
@@ -1,18 +1,14 @@
 
 macro(Roco2Configuration CONF )
-    add_executable(roco2_${CONF} ${CONF}/experiment.cpp)
+    add_executable(roco2_${CONF} experiment.cpp)
 
     target_link_libraries(roco2_${CONF} ${LAPACK_LIBRARIES} ${LIBNUMA_LIBRARIES} ${MKL_LIBRARIES} fastcpufreq ${CPUFREQ_LIBRARIES} roco2_core)
-
-    set_target_properties(roco2_${CONF} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/${CONF}")
 
     if(X86Adapt_FOUND)
         target_link_libraries(roco2_${CONF} ${X86ADAPT_LIBRARIES})
     endif()
 
     add_scorep_instrumentation(roco2_${CONF})
-    set_target_properties(roco2_${CONF}_scorep PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/${CONF}")
-
 
     install(TARGETS roco2_${CONF} RUNTIME DESTINATION bin)
 endmacro()
