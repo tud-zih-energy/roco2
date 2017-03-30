0ad2f8814ef6aaf6cf6655d2df468d1be44d84b6
Mario Bielert <mario.bielert@tu-dresden.de>
2017-03-30 18:55:28 +0200

    Adds firestarter cmake magic

---
+++ cmake/BuildFirestarter.cmake
@@ -0,0 +1,33 @@
+
+FILE(GLOB_RECURSE FS_SRC_FILES lib/FIRESTARTER/*)
+
+add_custom_command(
+    OUTPUT ${CMAKE_BINARY_DIR}/firestarter
+    COMMAND cmake -E make_directory ${CMAKE_BINARY_DIR}/firestarter
+    COMMENT "Create FIRESTARTER source code directory"
+    VERBATIM
+)
+
+add_custom_command(
+    OUTPUT ${CMAKE_BINARY_DIR}/firestarter/Makefile
+    COMMAND ${CMAKE_SOURCE_DIR}/lib/FIRESTARTER/code-generator.py -v
+    DEPENDS ${FS_SRC_FILES} ${CMAKE_BINARY_DIR}/firestarter
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/firestarter
+    COMMENT "Generate FIRESTARTER source code"
+    VERBATIM
+)
+
+add_custom_target(GenerateFirestarterSource ALL DEPENDS ${CMAKE_BINARY_DIR}/firestarter/Makefile)
+
+add_custom_command(
+    OUTPUT ${CMAKE_BINARY_DIR}/firestarter/libfirestarter.a
+    COMMAND make libfirestarter.a
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/firestarter
+    COMMENT "Build libfirestarter.a"
+    VERBATIM
+)
+
+add_custom_target(libfirestarter ALL DEPENDS ${CMAKE_BINARY_DIR}/firestarter/libfirestarter.a)
+
+set(FIRESTARTER_LIBRARIES ${CMAKE_BINARY_DIR}/firestarter/libfirestarter.a PARENT_SCOPE)
+set(FIRESTARTER_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/firestarter PARENT_SCOPE)
--- cmake/Roco2Configuration.cmake
+++ cmake/Roco2Configuration.cmake
@@ -2,7 +2,16 @@
 macro(Roco2Configuration CONF )
     add_executable(roco2_${CONF} experiment.cpp)
 
-    target_link_libraries(roco2_${CONF} ${LAPACK_LIBRARIES} ${LIBNUMA_LIBRARIES} ${MKL_LIBRARIES} fastcpufreq ${CPUFREQ_LIBRARIES} roco2_core)
+    target_link_libraries(
+        roco2_${CONF}
+        ${LAPACK_LIBRARIES}
+        ${LIBNUMA_LIBRARIES}
+        ${MKL_LIBRARIES}
+        fastcpufreq
+        ${CPUFREQ_LIBRARIES}
+        ${FIRESTARTER_LIBRARIES}
+        roco2_core
+    )
 
     if(X86Adapt_FOUND)
         target_link_libraries(roco2_${CONF} ${X86ADAPT_LIBRARIES})
