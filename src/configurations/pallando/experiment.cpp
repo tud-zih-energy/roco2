6b628188b1c6036a746e2f31808aa99e152be2df
Mario Bielert <mario.bielert@tu-dresden.de>
2024-02-16 16:13:19 +0100

    add fancy configuration for real this time

---
+++ src/configurations/pallando/experiment.cpp
@@ -0,0 +1,90 @@
+#include <roco2/initialize.hpp>
+
+#include <roco2/cpu/topology.hpp>
+
+#include <roco2/memory/numa.hpp>
+
+#include <roco2/chrono/chrono.hpp>
+#include <roco2/log.hpp>
+
+#include <roco2/experiments/const_length.hpp>
+#include <roco2/experiments/patterns/pattern_generator.hpp>
+
+#include <roco2/kernels/idle.hpp>
+#include <roco2/kernels/matmul.hpp>
+
+#include <roco2/task/experiment_task.hpp>
+#include <roco2/task/lambda_task.hpp>
+#include <roco2/task/task_plan.hpp>
+
+#include <string>
+#include <vector>
+
+using namespace roco2::experiments::patterns;
+
+void run_experiments(roco2::chrono::time_point& starting_point, bool eta_only) {
+    roco2::kernels::matmul mm;
+    roco2::kernels::idle idle;
+
+    roco2::memory::numa_bind_local nbl;
+
+    // ------ EDIT GENERIC SETTINGS BELOW THIS LINE ------
+
+    auto experiment_duration = std::chrono::seconds(120);
+
+    assert(omp_get_num_threads() == 104);
+
+    auto on_list =
+        block_pattern(1, false, triangle_shape::upper) >> stride_pattern(1, 52) >>
+        (triangle_pattern(0, 12) && triangle_pattern(13, 25) && triangle_pattern(26, 38) &&
+         triangle_pattern(39, 51)) >>
+        stride_pattern(1, 13) >> (triangle_pattern(0, 12) && triangle_pattern(13, 25)) >>
+        (triangle_pattern(0, 12) && triangle_pattern(13, 25) && triangle_pattern(27, 39)) >>
+        (triangle_pattern(0, 12) && triangle_pattern(13, 25) && triangle_pattern(27, 39) &&
+         triangle_pattern(52, 64)) >>
+        (triangle_pattern(0, 12) && triangle_pattern(13, 25) && triangle_pattern(52, 64)) >>
+        (triangle_pattern(0, 12) && triangle_pattern(13, 25) && triangle_pattern(52, 64) &&
+         triangle_pattern(65, 77));
+
+    // ------ EDIT GENERIC SETTINGS ABOVE THIS LINE ------
+
+    roco2::task::task_plan plan;
+
+#pragma omp master
+    { roco2::log::info() << "Experiment list has " << on_list.size() << " entries: " << on_list; }
+
+    auto experiment_startpoint =
+        roco2::initialize::thread(starting_point, experiment_duration, eta_only);
+
+    roco2::experiments::const_lenght exp(experiment_startpoint, experiment_duration);
+
+    auto experiment = [&](auto& kernel, const auto& on) {
+        plan.push_back(roco2::task::experiment_task(exp, kernel, on));
+    };
+
+    /* auto setting = [&](auto lambda) { plan.push_back(roco2::task::make_lambda_task(lambda)); };
+     */
+
+    // ------ EDIT TASK PLAN BELOW THIS LINE ------
+
+    // do one full idle
+    experiment(idle, roco2::experiments::cpu_sets::all_cpus());
+
+    for (const auto& on : on_list) {
+        experiment(mm, on);
+    }
+
+    // ------ EDIT TASK PLAN ABOVE THIS LINE ------
+
+#pragma omp master
+    {
+        roco2::log::info() << "ETA for whole execution: "
+                           << std::chrono::duration_cast<std::chrono::seconds>(plan.eta());
+    }
+
+    if (!eta_only) {
+#pragma omp barrier
+
+        plan.execute();
+    }
+}
